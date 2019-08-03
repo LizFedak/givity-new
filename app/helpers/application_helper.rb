581d5114938260f2ec139d96ed827e011a42ae79
Liz Fedak <liz@fernand-mbp.local>
2019-08-03 16:41:41 -0700

    going to add sendinblue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,5 +9,11 @@ module ApplicationHelper
         end
       end
 
+
+      def avatar_url(user)
+        gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+        "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=180"
+      end
+    
       
 end
