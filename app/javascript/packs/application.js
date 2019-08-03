baee72bc96803463732ea28b63ab9e72ca2802ca
Liz Fedak <liz@fernand-mbp.local>
2019-08-03 07:40:50 -0700

    navbar stuff

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,16 +2,22 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import 'bootstrap'
-import './src/application.scss'
+
 
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
 
+
+import 'bootstrap'
+import '../stylesheets/application'
+
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -20,5 +26,7 @@ require("jquery")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+require("jquery")
 require("trix")
 require("@rails/actiontext")
+
